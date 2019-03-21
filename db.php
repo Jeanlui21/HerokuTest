e44ba4b6a87b4e96fff13d323f334d52b74e87a9
jean lui <jean.ferrer@tecsup.edu.pe>
2019-03-20 22:31:32 -0500

    segundo commit

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 
 <?php
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'lab4';
+$server = 'us-cdbr-iron-east-03.cleardb.net';
+$username = 'b8841f10c91da9';
+$password = '3d5a9b16';
+$database = 'heroku_d6409e729c58c2a';
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
 ?>
+
